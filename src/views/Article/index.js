0cde207ff6e670d1f4f6b7d827f71d72aca10ba1
qh <2q@qhdeMacBook-Pro.local>
2019-05-21 23:58:17 +0800

    feat:文章管理页面的基本渲染

--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,48 +1,83 @@
 import React, { Component } from 'react'
 
-import { Card, Button, Table } from 'antd'
+import { Card, Button, Table, Tag } from 'antd'
 
 import { getArticles } from '../../requests'
+import moment from 'moment'
 
-const dataSource = [
-  {
-    key: '1',
-    name: '胡彦斌',
-    age: 32,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '2',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-]
+const ButtonGroup = Button.Group
 
-const columns = [
-  {
-    title: '姓名',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: '住址',
-    dataIndex: 'address',
-    key: 'address',
-  },
-]
+const titleColumnsMap = {
+  id: 'id',
+  title: '标题',
+  author: '作者',
+  createAt: '创建时间',
+  amount: '阅读量'
+}
 
 export default class ArticleList extends Component {
-  componentDidMount () {
+  constructor () {
+    super()
+    this.state = {
+      dataSource: [],
+      columns: [],
+      total: 0
+    }
+  }
+  createColumns = (columnsKeys) =>{
+    const columns = columnsKeys.map(item => {
+      if (item === 'amount') {
+        return {
+          title: titleColumnsMap[item],
+          key: item,
+          render: (record) => {
+            return <Tag color={record.amount > 230 ? "red" : "cyan"}>{record.amount}</Tag>
+          }
+        }
+      }
+      if (item === 'createAt') {
+        return {
+          title: titleColumnsMap[item],
+          key: item,
+          render: (record) => {
+            return moment(record.createAt).format('YYYY年MM月DD日 HH:mm:ss')
+          }
+        }
+      }
+      return {
+        title: titleColumnsMap[item],
+        dataIndex: item,
+        key: item
+      }
+    })
+    columns.push({
+      title: '操作',
+      key: 'action',
+      render: () => {
+        return (
+          <ButtonGroup>
+            <Button size="small" type="primary">编辑</Button>
+            <Button size="small" type="danger">删除</Button>
+          </ButtonGroup>
+        )
+      }
+    })
+    return columns
+  }
+  getData = () => {
     getArticles()
       .then(resp => {
-        console.log(resp)
+        const columnsKeys = Object.keys(resp.list[0])
+        const columns = this.createColumns(columnsKeys)
+        this.setState({
+          total: resp.total,
+          dataSource: resp.list,
+          columns
+        })
       })
+  }
+  componentDidMount () {
+    this.getData()
   }
   render() {
     return (
@@ -53,8 +88,9 @@ export default class ArticleList extends Component {
           extra={<Button>导出Excel</Button>} 
         >
           <Table 
-            dataSource={dataSource} 
-            columns={columns} 
+            rowKey={record => record.id}
+            dataSource={this.state.dataSource} 
+            columns={this.state.columns} 
             pagination={{
               hideOnSinglePage: true
             }}
